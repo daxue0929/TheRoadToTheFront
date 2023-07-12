5dee27ecc6931e5a91c1c8f89eb87e29ae96b1a7
daxuewwang <daxuewwang@tencent.com>
2023-07-12 23:56:05 +0800

    [ChromeApp][devStartFirst] 修改标题

--- projects/book-web/src/pages/index/index.vue
+++ projects/book-web/src/pages/index/index.vue
@@ -1,10 +1,7 @@
 <template>
-	<view class="content">
-    <image class="logo" src="../../static/logo.png"></image>
-		<view>
-      <text class="title">{{title}}</text>
-    </view>
-	</view>
+  <view>
+    你好世界
+  </view>
 </template>
 
 <script lang="ts">
@@ -26,26 +23,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
