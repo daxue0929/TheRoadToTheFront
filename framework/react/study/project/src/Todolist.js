39c90ce833a661a9fdef2e58f68cf0e391747a97
daxue0929 <daxue0929@qq.com>
2024-07-14 21:07:18 +0800

    [framework][react][study][project] 修改demo Todolist组件

--- framework/react/study/project/src/Todolist.js
+++ framework/react/study/project/src/Todolist.js
@@ -1,36 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import store from './store'
 
 export default class Todolist extends Component {
 
 	constructor(props) {
 		super(props)
-
 		this.state = store.getState()
-
-		this.handleInputChange = this.handleInputChange.bind(this)
 	}
 
-	handleInputChange(e) {
+	handleInputChange = (e) => {
+		const { value } = e.target;
 		const action = {
 			type: 'change_input',
 			value: e.target.value
 		}
+		this.setState({ inputValue: value });
 		console.log(action)
+	}
+
+	submitItem = () => {
+		const { inputValue, list } = this.state;
+		// 将输入框的值添加到 list 中
+		this.setState({
+			inputValue: '',
+			list: [...list, inputValue] // 使用 setState 更新 list 状态
+		});
 	}
 
 
 	render() {
+		const { inputValue, list } = this.state;
 		return (
 			<div>
-				<input value={this.state.inputValue} type="text"
+				<input value={inputValue} type="text"
 							 onChange={this.handleInputChange}
 				/>
-				<button>提交</button>
+				<button onClick={this.submitItem}>提交</button>
 				<ul>
-					<li>1</li>
-					<li>2</li>
-					<li>3</li>
+					{list.map((item, index) => (
+						<li key={index}>{item}</li>
+					))}
 				</ul>
 			</div>
 		)
