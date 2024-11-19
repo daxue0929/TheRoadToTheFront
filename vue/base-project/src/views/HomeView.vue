45ac479fe19acceff0df0dea21218355a50dfc7c
xuedi.wang <wangxuedi3319@>
2024-11-19 18:02:09 +0800

    tijiao

--- vue/base-project/src/views/HomeView.vue
+++ vue/base-project/src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import {hello} from '@/api/hello'
 
-const fetchHello = async () => {
-  return await hello({id: 2, name: 'test'});
-}
-
-fetchHello().then(resp => {
-  console.log("id: ", resp.id)
-  console.log("name: ", resp.name)
-})
+// const fetchHello = async () => {
+//   return await hello({id: 2, name: 'test'});
+// }
+//
+// fetchHello().then(resp => {
+//   console.log("id: ", resp.id)
+//   console.log("name: ", resp.name)
+// })
 
 console.log("import.meta.env.BASE_URL: ", import.meta.env.BASE_URL)
 
