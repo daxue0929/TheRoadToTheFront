6239af0921f89886fe24a699be4bd60ad31c9520
daxue0929 <wangxuedi3319@>
2024-12-03 17:36:47 +0800

    react-static初始化项目

---
+++ framework/react/study/static-website/src/pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Profile from '../components/Profile.js';
+import { getImageUrl } from '../utils/util'
+
+function Avatar({ person, size }) {
+    return (
+        <img
+            className="avatar"
+            src={getImageUrl(person)}
+            alt={person.name}
+            width={size}
+            height={size}
+        />
+    );
+}
+
+function Card({ children }) {
+    return (
+        <div className="card">
+            {children}
+        </div>
+    );
+}
+
+export default () => (
+    <div>
+        <h1>Welcome to React-Static</h1>
+        <section>
+            <h2>Amazing scientists</h2>
+            <Profile />
+        </section>
+        <hr />
+
+        <Card>
+            <Avatar size={100} person={{
+                name: 'Katsuko Saruhashi',
+                imageId: 'YfeOqp2'
+            }} />
+        </Card>
+    </div>
+
+)
